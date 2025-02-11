8dd8050bb036e22ae875577e1b7cd99512fb405a
mehedi-15 <mehedi4794@gmail.com>
2025-02-11 15:57:53 +0600

    update loader

--- src/Componenet/Loader.jsx
+++ src/Componenet/Loader.jsx
@@ -3,7 +3,11 @@ import { HashLoader } from "react-spinners";
 
 const Loader = ({ loading }) => (
   <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "calc(100vh - 100px)" }}>
-    <HashLoader color="#3B82F6" loading={loading} size={150} />
+    {/* <HashLoader color="#3B82F6" loading={loading} size={150} /> */}
+    <span className="loading loading-bars loading-xs"></span>
+<span className="loading loading-bars loading-sm"></span>
+<span className="loading loading-bars loading-md"></span>
+<span className="loading loading-bars loading-lg"></span>
   </div>
 );
 
