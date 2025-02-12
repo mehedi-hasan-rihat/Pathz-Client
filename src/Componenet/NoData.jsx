3e6f4427cdbf88500e5d4a8d60c1b3cab1e239c1
mehedi-15 <mehedi4794@gmail.com>
2025-02-12 21:56:30 +0600

    add text in no data

--- src/Componenet/NoData.jsx
+++ src/Componenet/NoData.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Empty from '../assets/Empty.svg'
 export default function NoData() {
   return (
-    <div className="h-64  py-10 mx-auto bg-[#ECF5FF] w-[80%] flex items-center justify-center rounded-md">
+    <div className="h-64  py-10 mx-auto bg-[#e7f2ff] w-[80%] flex items-center justify-center rounded-md">
     <img src={Empty} alt="" className="h-[80%]" />
-    <p className="text-xl font-semibold">Sorry ! No Data Found.</p>
+    <p className="text-xl font-semibold">Sorry ! No Data Found Try Again.</p>
   </div>
   )
 }
